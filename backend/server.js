452d754efcac70d6988380045c768b63bb7b1859
Thanos-83 <s_sbonias@hotmail.com>
2023-03-09 14:48:40 +0200

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,13 @@ app.use('/api/dashboard/categories', categoryRoutes);
 app.use('/api/dashboard/orders', adminOrderRoutes);
 app.use('/api/dashboard/users', adminUserRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
